aaeb1404c6fd9a88391e04ee79fd3c7a1c51c988
Carina Rodriguez <141369717+carinabrodriguez@users.noreply.github.com>
2023-09-03 22:10:16 -0300

    addEventListener

---
+++ eventos.js
@@ -0,0 +1,7 @@
+// Selecciona el div por su id
+const elDiv = document.getElementById("elDiv");
+
+// Agrega un manejador de eventos para el evento "click"
+elDiv.addEventListener("click", function() {
+    alert("Hola! Soy el div");
+});
